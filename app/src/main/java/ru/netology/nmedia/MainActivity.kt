aaa3344c4e93f43d647dff6b36ef26c867936b7b
Sergey <sergey@localhost>
2021-12-16 23:05:20 +0300

    mvvm

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,8 +3,11 @@ package ru.netology.nmedia
 import android.icu.text.DecimalFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.repository.PostRepositoryInMemory
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,62 +15,42 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36"
-        )
+        val viewModel: PostViewModel by viewModels()
 
         with(binding) {
-            author.text = post.author
-            content.text = post.content
-            published.text = post.published
+            viewModel.get().observe(this@MainActivity) { post ->
 
-            initLikes(post)
-            initShare(post)
+                author.text = post.author
+                content.text = post.content
+                published.text = post.published
 
-            like.setOnClickListener {
-                setLike(post)
-            }
-            numbersOfLikes.setOnClickListener {
-                setLike(post)
-            }
+                like.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_liked else R.drawable.ic_like
+                )
 
-            share.setOnClickListener {
-                setShare(post)
-            }
-            numbersOfShared.setOnClickListener {
-                setShare(post)
+                initLikes(post)
+                initShare(post)
+
+                like.setOnClickListener {
+                    viewModel.like()
+                }
+                numbersOfLikes.setOnClickListener {
+                    viewModel.like()
+                }
+
+                share.setOnClickListener {
+                    viewModel.share()
+                }
+                numbersOfShared.setOnClickListener {
+                    viewModel.share()
+                }
             }
         }
-    }
-
-    private fun ActivityMainBinding.setLikeImage(post: Post) {
-        like.setImageResource(
-            if (post.likedByMe) {
-                R.drawable.ic_liked
-            } else {
-                R.drawable.ic_like
-            }
-        )
     }
 
     private fun ActivityMainBinding.initLikes(post: Post) {
         numbersOfLikes.text = formatNumbers(post.likes)
     }
-
-    private fun ActivityMainBinding.setLike(post: Post) {
-        post.likedByMe = !post.likedByMe
-        setLikeImage(post)
-        if (post.likedByMe) {
-            post.likes++
-        } else {
-            post.likes--
-        }
-        numbersOfLikes.text = formatNumbers(post.likes)
-    }
-
 
     private fun ActivityMainBinding.initShare(post: Post) {
         numbersOfShared.text = formatNumbers(post.share)
@@ -84,19 +67,13 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun ActivityMainBinding.setShare(post: Post) {
-        post.share++
-        setShareImage(post)
-        numbersOfShared.text = formatNumbers(post.share)
-    }
-
     private fun formatNumbers(num: Int): String {
         val dec = DecimalFormat("#.#")
 
         return if (num <= 999) {
             num.toString()
-        } else if (num <= 999_999) {
-           dec.format(num / 1000f).toString() + "k"
+        } else if (num < 999_999) {
+            dec.format(num / 1000f).toString() + "k"
         } else {
             dec.format(num / 1_000_000f).toString() + "M"
         }
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -6,6 +6,6 @@ data class Post(
     val content: String = "",
     val published: String = "",
     var likedByMe: Boolean = false,
-    var likes: Int = 999,
-    var share: Int = 985
+    val likes: Int = 0,
+    val share: Int = 1000
 )
